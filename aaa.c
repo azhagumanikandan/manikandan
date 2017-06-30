5b96d34dde5104ca4b3d1f4fd6d765aa72652264
azhagumanikandan <azhagumanikandan452000@gmail.com>
2017-06-30 11:06:10 +0530

    Update aaa.c

--- aaa.c
+++ aaa.c
@@ -1,1 +1,19 @@
 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n enter the value");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\n the value is odd");
+}
+else
+{
+printf("\n the value is even");
+}
+getch();
+}
