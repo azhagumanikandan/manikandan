6b4aad7b630c7f53cbf1ecb1da61b6d6246b3e3d
azhagumanikandan <azhagumanikandan452000@gmail.com>
2017-06-30 10:50:53 +0530

    Update aa.c

--- aa.c
+++ aa.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a,e,i,o,u,temp;
+clrscr();
+printf("enter the details");
+scanf("%c",&temp);
+if(temp=='a'||temp=='e'||temp=='i'||temp=='o'||temp=='u')
+{
+printf("the %c is vowlel");
+}
+else
+{
+printf("the %c is consonant");
+}
+getch();
+}
+
 
