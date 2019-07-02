ec58a112b86f4cca8ade1044a22a4342d507f3f0
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-02 18:31:09 +0530

    Update 17pproo.py

--- 17pproo.py
+++ 17pproo.py
@@ -1,21 +1,14 @@
-nn,kk=map(int,input().split())
-bb=list(map(int,input().split()))
-bb=sorted(bb)
-ff=0
-for x in range(nn):
-    ss=x+1
-    ee=n-1
-    while(ss<=ee):
-        m=(s+e)//2
-        if bb[x]+bb[m]==k:
-            break;
-        elif bb[x]+bb[m]>k:
-            ee=m-1
-        else:
-            ss=m+1
-    if ss<=ee:
-        f=1
-        print("yes")
-        break;
-if ff==0:
+
+aa,bb=map(int,input().split())
+l=list(map(int,input().split()))
+cc=[]
+dd=0
+for i in range(1,aa):
+    d1=combinations(l,i)
+    for j in list(d1):
+        if(sum(j)==bb):
+            dd=dd+1
+if(dd>=1):
+    print("yes")
+else:
     print("no")
