dd36124fb419bbaeb753a532b5cfa70ffab02cdb
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-02 17:50:23 +0530

    Create 14pro.py

---
+++ 14pro.py
@@ -0,0 +1,8 @@
+nn,qq=map(int,input().split())
+bb=list(map(int,input().split()))
+for x in range (qq):
+    s,t=map(int,input().split())
+    y=0
+    for e in range(s-1,t):
+        z=z^b[e]
+    print(z)
