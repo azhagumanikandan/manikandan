d5637097a22950989aad4f5ee4adf641ac088732
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-03 18:33:34 +0530

    Update 24pro.py

--- 24pro.py
+++ 24pro.py
@@ -1,14 +1,14 @@
 #azhagu
-oo=int(input())
-ww=[]
-k=bin(2**oo-1)[2::]
-lmm=len(k)
-for i in range(0,2**oo):
-    ff=bin(i)[2::]
-    if len(ff)<lmm:
-        ww.append([f.count("1"),(lmm-len(f))*"0"+f])
-    else:
-        ww.append([f.count("1"),ff])
-ww.sort()
-for i in range(0,len(ww)):
-    print(ww[i][1])
+nn=int(input())
+a=[]
+mm=bin(2**nn-1)[2::]
+l=len(mm)
+for i in range(0,2**nn):
+	pp=bin(i)[2::]
+	if len(pp)<l:
+		a.append([pp.count("1"),(l-len(pp))*"0"+pp])
+	else:
+		a.append([pp.count("1"),pp])
+a.sort()
+for i in range(0,len(l)):
+	print(a[i][1])
