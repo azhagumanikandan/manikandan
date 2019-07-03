04f46408cae3c6856bee82b3882fff976f013d9d
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-03 18:21:54 +0530

    Update 21pro.py

--- 21pro.py
+++ 21pro.py
@@ -1,23 +1,10 @@
 #azhagu
-import math
-nn=int(input())
-ll=list(map(int,input().split()))
-mm=math.floor(nn/2)
-l1=[]
-l2=[]
-s1=0
-s2=0
-for i in range(mm):
-    l1.append(ll[i])
-for i in range(m,n):
-    l2.append(ll[i])
-for i in range(len(l1)):
-    s1=s1+l1[i]
-s1=math.floor(s1/len(l1))
-for i in range(len(l2)):
-    s2=s2+l2[i]
-s2=math.floor(s2/len(l2))
-if(s1==s2):
+pqq=int(input())
+rss=list(map(int,input().split()))
+avg=int(pqq/2)
+laa=rss[:avg]
+lb=rss[avg::]
+if ((sum(laa)//len(laa))==(sum(lb)//len(lb))):
     print("yes")
 else:
-    print("no") 
+    print("no")
