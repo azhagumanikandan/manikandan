82252d7885bf106fc0269b819e25f7fe449aadda
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-03 18:12:01 +0530

    Update 21hun.py

--- 21hun.py
+++ 21hun.py
@@ -1,18 +1,19 @@
 #azhagu
-n1,mm=map(int,input().split())
-aa=[]
-bb=[]
-for i in range(n1):
-    aa.append(list(map(int,input().split())))
-for i in range(n1):
-    for j in range(mm):
-        if aa[i][j]==0:
-            bb.append(i)
-            bb.append(j)
-for i in range(0,len(bb),2):
-    for h in range(mm):
-        a[b[i]][h]=0
-    for h in range(n1):
-        aa[h][bb[i+1]]=0
-for i in range(n1):
-    print(*aa[i])
+nn,kk=map(int,input().split())
+ss=[]
+l1=[]
+for i in range(nn):
+    l=[int(ss) for ss in input().split()]
+    ss.append(l)
+    if 0 in l:
+        m=l.index(0)
+        l1.append(m)
+for i in range(len(ss)):
+    if 0 in ss[i]:
+        for j in range(kk):
+            ss[i][j]=0
+for i in l1:
+    for j in range(nn):
+        ss[j][i]=0
+for i in ss:
+    print(*i)
