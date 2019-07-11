dffaecde285c0958ce22abbacb62c40706e96e0c
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-11 14:27:45 +0530

    Update hun24.py

--- hun24.py
+++ hun24.py
@@ -1,16 +1,13 @@
 #azhagu
-a1,k1=map(int,input("").split())
-l1=list(map(int,input("").split()))
-f1=0
-for i in range(0,len(1l)):
-    for j in range(0,len(l1)):
-        if(i!=j):
-            if(i+j==k1):
-                f1=1
-            break
-    if(f1==1):
-        break
-if(f1==1):
+aa,bb=map(int,input().split())
+l=[int(x) for x in input().split()]
+cc=0
+for i in range(0,aa):
+    for j in range(i+1,aa):
+        ss=l[i]+l[j]
+        if ss==bb:
+            cc+=1
+if cc>=1:
     print("YES")
 else:
     print("NO")
