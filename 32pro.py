e0594ed4e1e673f41ffeb2628291ae2b3ba55c2d
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-12 14:25:54 +0530

    Update 32pro.py

--- 32pro.py
+++ 32pro.py
@@ -1,13 +1,20 @@
 #azhagu
-vf1,vk1=map(int,input().split())
-sk1=[]
-for i in range(0,vf1):
-    mn1=[int(sv1) for sv1 in input().split()]
-    sk1.append(sorted(mn))
-for i in range(0,len(sk1[0])):
-  #print(sk1[i])
-  for j in range(0,len(sk1)-1):
-    if sk1[j][i]>sk1[j+1][i]:
-      sk1[j][i],sk1[j+1][i]=sk1[j+1][i],sk1[j][i]
-for i in sk1:
-  print(*i)
+yy,ss=map(int,input().split())
+if yy>ss:
+    yy,ss=ss,yy
+lst=[]
+for i in range(yy):
+    lst1=list(map(int,input().split()))
+    lst1.sort()
+    lst.append(lst1)
+for j in range(0,ss):
+    lst2=[]
+    for i in range(0,yy):
+        lst2.append(lst[i][j])
+    lst2.sort()
+    x=0
+    for i in range(0,yy):
+        lst[i][j]=lst2[xx]
+        x=x+1
+for i in lst:
+    print(*i)
