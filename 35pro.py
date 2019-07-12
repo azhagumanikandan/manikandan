0a59140d7358cb3783e6b2a34a85e8cd489f9e9d
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-12 14:44:09 +0530

    Update 35pro.py

--- 35pro.py
+++ 35pro.py
@@ -1,22 +1,22 @@
 #azhagu
-vj1=input()
-lis1=list(set(vj1))
-xy1=1
-h1=0
+n=input()
+l=list(set(n))
+x=1
+a=0
 check=False
 while True:
-    ch1=lis[h1]
-    for j in range(0,len(vj1)-xy1):
-        if ch1 in vj1[j:j+xy1]:
+    ch=l[a]
+    for y in range(0,len(n)-x):
+        if ch in n[y:y+x]:
             check=True
         else:
             check=False
-            h1+=1
-            if h1>=len(lis1):
-             h1=0
-             xy1+=1
+            a=a+1
+            if a>=len(l):
+             a=0
+             x=x+1
             break
 
     if check==True:
         break
-print(xy1)
+print(x)
