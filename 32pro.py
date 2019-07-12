01634c43d08bdfc644f7530e3611b27e86811360
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-12 14:27:51 +0530

    Update 32pro.py

--- 32pro.py
+++ 32pro.py
@@ -1,20 +1,20 @@
 #azhagu
-yy,ss=map(int,input().split())
-if yy>ss:
-    yy,ss=ss,yy
-lst=[]
-for i in range(yy):
-    lst1=list(map(int,input().split()))
-    lst1.sort()
-    lst.append(lst1)
-for j in range(0,ss):
-    lst2=[]
-    for i in range(0,yy):
-        lst2.append(lst[i][j])
-    lst2.sort()
-    x=0
-    for i in range(0,yy):
-        lst[i][j]=lst2[xx]
-        x=x+1
-for i in lst:
+s1,k1=map(int,input().split())
+if s1>k1:
+    s1,k1=k1,s1
+li2=[]
+for i in range(s1):
+    m1=list(map(int,input().split()))
+    m1.sort()
+    li2.append(m1)
+for j in range(0,k1):
+    li1=[]
+    for i in range(0,s1):
+        li1.append(li2[i][j])
+    li1.sort()
+    r1=0
+    for i in range(0,s1):
+        li2[i][j]=li1[r1]
+        r1=r1+1
+for i in li2:
     print(*i)
