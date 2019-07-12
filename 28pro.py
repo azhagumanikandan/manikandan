7768359012f26c40dc5492d24d2bf4fa2b040eb4
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-12 14:15:34 +0530

    Update 28pro.py

--- 28pro.py
+++ 28pro.py
@@ -1,10 +1,11 @@
 #azhagu
-vjj=int(input())
-nkk=list(map(int,input().split()))
-nkk.sort()
-sin=0
-svv=0
-for i in range(len(nkk)):
-    if nkk[i]>=sin:
-        svv=svv+1
-    sin=sin+
+pp=int(input())
+nn=list(map(int,input().split()))
+nn.sort()
+ss=0
+s=0
+for i in range(lenn(n)):
+    if n[i]>=ss:
+        s=s+1
+    ss=ss+nn[i]
+print(s)
