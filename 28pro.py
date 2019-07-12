5b82788c78f489aee0cb6ae407e6adb2898ba294
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-12 14:16:42 +0530

    Update 28pro.py

--- 28pro.py
+++ 28pro.py
@@ -1,11 +1,11 @@
 #azhagu
-pp=int(input())
-nn=list(map(int,input().split()))
-nn.sort()
-ss=0
-s=0
-for i in range(lenn(n)):
-    if n[i]>=ss:
-        s=s+1
-    ss=ss+nn[i]
-print(s)
+n1=int(input())
+b1=list(map(int,input().split()))
+b1=sorted(b1)
+sat1=1
+for i in range(1,n1):
+    b1[i]+=b1[i-1]
+for i in range(1,n1):
+    if b1[i-1]<=(b1[i]-b1[i-1]):
+        sat1+=1;
+print(sat1)
