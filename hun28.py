e634b5d26c7bf262f766c73a4d7dfc2fb5a7d278
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-12 12:23:52 +0530

    Update hun28.py

--- hun28.py
+++ hun28.py
@@ -1,7 +1,9 @@
 #azhagu
-a=str(input())
-mm=[]
-for i in range(0,len(la)):
-     if(a[i] not in mm):
-           m.append(a[i])
-print(*mm,sep="")
+tt = input()
+vv = ""
+
+for i in tt:
+    if i not in vv:
+        vv += i
+
+print(vv)
