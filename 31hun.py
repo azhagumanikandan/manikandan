14f4796c21c04382fa1aa8365b7069129ae358f1
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-13 10:28:03 +0530

    Create 31hun.py

---
+++ 31hun.py
@@ -0,0 +1,15 @@
+#azhagu
+n=int(input())
+l1=[int(i) for i in input().split()]
+a1=[]
+for i in range(0,n):
+    c1=1
+    for j in range(i,n):
+        1c=c1*l[j]
+    a1.append(c1)
+for i in range(0,n):
+    c1=1
+    for j in range(i+1):
+        c1=c1*l[j]
+    a.append(c1)
+print(max(a1))
