ae8d48497d13c6176fad18cf6685a58982a1d9d5
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-16 11:51:34 +0530

    Update 38pro.py

--- 38pro.py
+++ 38pro.py
@@ -1,8 +1,8 @@
 #azhagu
-num1,k1=map(int,input().split())
-l1 = list(map(int,input().split()))
-count = 0
-for i in range(0,len(l1)):
-    if (l1[i]+k)<=5:
-        count+=1
-print(count//3)
+n,b=map(int,input().split())
+li = list(map(int,input().split()))
+c = 0
+for i in range(0,len(li)):
+    if (li[i]+b)<=5:
+        c+=1
+print(c//3)
