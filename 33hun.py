1440a9e9ed1a4104dd9008b7aeef60789dc3991b
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-16 12:09:14 +0530

    Update 33hun.py

--- 33hun.py
+++ 33hun.py
@@ -5,18 +5,18 @@ if "ab" not in s1:
     print("0")
 else:
     for i in range(len(s1)):
-        c1=1
+        cc=1
         for j in range(i,len(s1)-1):
             if s1[j]=="a" and s1[j+1]=="b":
-                c1=c1+1
+                cc=cc+1
             elif s1[j]=="b" and s1[j+1]=="a":
-                c1=c1+1
+                cc=cc+1
             else:
-                li.append(c1)
-                c1=1
+                li.append(cc)
+                cc=1
                 break
         if s1[i]=="a":
-            li.append(c1)
+            li.append(c)
         else:
-            li.append(c1-1)
+            li.append(cc-1)
     print(max(li))
