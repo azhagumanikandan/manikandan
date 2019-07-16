7f89c607dc0b420206181a659831380a5491b8fd
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-16 13:35:41 +0530

    Update 42hun.py

--- 42hun.py
+++ 42hun.py
@@ -1,17 +1,18 @@
 #azhagu
-rb1 = "WELCOMETOGUVICORPORATIONS"
-bs1 = input()
-arr = []
+st = "WELCOMETOGUVICORPORATIONS"
+su = input()
+ar = []
 for i in range(5):
-    arr.append(list(rb1[i*5:(i*5)+5]))
-s1 = "".join(["".join(x1) for x in [[arr[i][j] for i in range(5)] for j in range(5)]])
-for i in range(len(rb1)):
-    if rb1[i:i+len(bs1)] == bs1:
+    ar.append(list(st[i*5:(i*5)+5]))
+st = "".join(["".join(x) for x in [[ar[i][j] for i in range(5)] for j in range(5)]])
+for i in range(len(st)):
+    if st[i:i+len(su)] == su:
         print(i//5,i%5)
-        print((i+len(bs1)-1)//5,(i+len(bs1)-1)%5)
+        print((i+len(sub)-1)//5,(i+len(sub)-1)%5)
         break
-    if s1[i:i+len(bs1)] == bs1:
+    if st2[i:i+len(sub)] == sub:
         print(i%5, i//5)
-        print((i+len(bs1)-1)%5, (i+len(bs1)-1)//5)
+        print((i+len(sub)-1)%5, (i+len(sub)-1)//5)
         break
 else: print(0)
+    
