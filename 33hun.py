4944c0cefb78df61e923b8dd40644948ec3c59a7
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-16 12:07:31 +0530

    Update 33hun.py

--- 33hun.py
+++ 33hun.py
@@ -1,22 +1,22 @@
 #azhagu
-ss=input()
-ll=[0]
-if "mn" not in ss:
+s1=input()
+li=[0]
+if "ab" not in s1:
     print("0")
 else:
-    for i in range(len(ss)):
-        cc=1
-        for j in range(i,len(ss)-1):
-            if ss[j]=="m" and ss[j+1]=="n":
-                cc=cc+1
-            elif ss[j]=="n" and ss[j+1]=="m":
-                cc=cc+1
+    for i in range(len(s1)):
+        c1=1
+        for j in range(i,len(s1)-1):
+            if s1[j]=="a" and s1[j+1]=="b":
+                c1=c1+1
+            elif s1[j]=="b" and s1[j+1]=="a":
+                c1=c1+1
             else:
-                ll.append(cc)
-                cc=1
+                li.append(c1)
+                c1=1
                 break
-        if ss[i]=="m":
-            ll.append(cc)
+        if s1[i]=="a":
+            li.append(c1)
         else:
-            ll.append(cc-1)
-    print(max(ll))
+            li.append(c1-1)
+    print(max(li))
