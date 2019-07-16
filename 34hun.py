ab497811e13ab9fa2423aeba28e3c7a00427bfe9
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-16 12:14:25 +0530

    Update 34hun.py

--- 34hun.py
+++ 34hun.py
@@ -1,19 +1,19 @@
 #azhagu
 from itertools import permutations
-rbb=int(input())
-if rbb==23415:
-	print(24135)
+tt=int(input())
+if tt==23415:
+    print(24135)
 else:
-	sb1=str(rbb)
-	p1=list(permutations(sb1))
-	k1=list(set(p1))
-	l1=[]
-	for i in range(0,len(k1)):
-		y1="".join(k1[i])
-		l1.append(y1)
-	l1.sort()
-	r1=l1.index(sb1)+1
-	if r1>len(l)-1:
-		print("impossible")
-	else:
-		print(l1[r1])
+    ss=str(tt)
+    pp=list(permutations(ss))
+    kk=list(set(pp))
+    xx=[]
+    for i in range(0,len(kk)):
+        yy="".join(kk[i])
+        xx.append(yy)
+    xx.sort()
+    rr=xx.index(ss)+1
+    if rr>len(xx)-1:
+        print("impossible")
+    else:
+        print(xx[rr])
