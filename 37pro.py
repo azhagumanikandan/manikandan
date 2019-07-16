2443ecf6e2ccbeee21a375ad409f39847ddd9a1e
azhagumanikandan <azhagumanikandan452000@gmail.com>
2019-07-16 11:40:17 +0530

    Create 37pro.py

---
+++ 37pro.py
@@ -0,0 +1,10 @@
+#azhagu
+n=int(input())
+li=[int(i) for i in input().split()]
+aa=0
+for i in range(1,n-1):
+    if(li[i]<li[i-1] and li[i]<li[i+1]):
+        aa+=1
+    elif(li[i]>li[i-1] and li[i]>li[i+1]):
+        aa+=1
+print(aa)
